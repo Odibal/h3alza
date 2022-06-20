456b29beea48705fd0b17ca7fc0b1fb30a4ff318
Maximilián Kompas <71879928+Odibal@users.noreply.github.com>
2022-06-20 08:59:39 +0200

    Update Plugin.php

--- h3alza/plugins/max/firm/Plugin.php
+++ h3alza/plugins/max/firm/Plugin.php
@@ -23,31 +23,6 @@ class Plugin extends PluginBase
         ];
     }
 
-    /**
-     * Register method, called when the plugin is first registered.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-    }
-
-    /**
-     * Boot method, called right before the request route.
-     *
-     * @return array
-     */
-    public function boot()
-    {
-
-    }
-
-    /**
-     * Registers any front-end components implemented in this plugin.
-     *
-     * @return array
-     */
     public function registerComponents()
     {
         return []; // Remove this line to activate
@@ -57,11 +32,6 @@ class Plugin extends PluginBase
         ];
     }
 
-    /**
-     * Registers any back-end permissions used by this plugin.
-     *
-     * @return array
-     */
     public function registerPermissions()
     {
         return []; // Remove this line to activate
@@ -74,11 +44,6 @@ class Plugin extends PluginBase
         ];
     }
 
-    /**
-     * Registers back-end navigation items for this plugin.
-     *
-     * @return array
-     */
     public function registerNavigation()
     {
         return [
